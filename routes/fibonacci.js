17b0342d2c5d05b19dad9362d326e5f4ea0540fd
Sasa Macakanja <sasa.macakanja@gmail.com>
2017-01-10 15:34:28 +0100

    added naive implementation of Fibonacci algorithm - computationally intensive code for event loop

--- routes/fibonacci.js
+++ routes/fibonacci.js
@@ -8,6 +8,8 @@ router.get('/', (req, res, next) => {
 
   let parsedInput = parseInt(req.query.fibonum, 10);
 
+  // This type of event handler is computationally very intensive and
+  // prevents the system from processing requests, it block NodeJS event loop.
   if (parsedInput && util.isNumber(parsedInput) && parsedInput >= 1) {
     // Calculate Fibonacci number directly in this server
     res.render('fibonacci', {
