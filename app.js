e0bf79606c1f602a5dd3fd2c4f584a5042413b26
Sasa Macakanja <sasa.macakanja@gmail.com>
2017-01-10 13:47:31 +0100

    added myLogger middleware

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ var users = require('./routes/users');
 
 var app = express();
 
+var myLogger = (req, res, next) => {
+  console.log('CUSTOM LOGGER: ', req.path);
+  next();
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(myLogger);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
