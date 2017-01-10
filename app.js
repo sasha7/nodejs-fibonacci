501b0fecbe51f4cda3cd1627445468aeaaec117f
Sasa Macakanja <sasa.macakanja@gmail.com>
2017-01-10 13:58:52 +0100

    added requestTime middleware

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ var myLogger = (req, res, next) => {
   next();
 };
 
+var requestTime = (req, res, next) => {
+  req.requestTime = Date.now();
+  next();
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,6 +27,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(myLogger);
+app.use(requestTime);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
