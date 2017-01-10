a8d05c7c52cc45efb26e19f4378565f65964c077
Sasa Macakanja <sasa.macakanja@gmail.com>
2017-01-10 14:52:28 +0100

    added POST route for /users

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', time: new Date(req.requestTime) });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/:id', function(req, res, next) {
+  res.send('respond with a resource with ID: ' + req.params.id);
+});
+
+// Example usage:
+// http POST http://localhost:3000/users Content-Type=application/json id=2
+// curl -X POST -H 'Content-Type: application/json' -d '{"id": "2"}' http://localhost:3000/users
+router.post('/', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    id: req.body.id,
+    username: 'sasham'
+  }));
 });
 
 module.exports = router;
